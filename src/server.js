ccb0c0e269ab5520feff4dec8b8a888ac8809667
Erik Yamamoto <erik.ymmt@gmail.com>
2022-10-12 19:21:41 -0300

    comment mySQL connection verification

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
-const connection = require('./models/connection');
+// const connection = require('./models/connection');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, async () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
+  // const [result] = await connection.execute('SELECT 1');
+  // if (result) {
+  //   console.log('MySQL connection OK');
+  // }
 });
